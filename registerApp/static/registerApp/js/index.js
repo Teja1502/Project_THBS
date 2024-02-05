0ffc7adc19f8ad9b5d3052db5ecdadefea4cc417
teja <teja_samanthavada@thbs.india.com>
2024-02-05 17:35:20 +0530

    Final Version

--- registerApp/static/registerApp/js/index.js
+++ registerApp/static/registerApp/js/index.js
@@ -11,20 +11,19 @@ const getBooks = async (book) => {
 };
 
 const extractThumbnail = ({ imageLinks }) => {
-  const DEFAULT_THUMBNAIL = "icons/logo.svg";
+  const DEFAULT_THUMBNAIL = "../../static/registerApp/images/default_book.png";
   if (!imageLinks || !imageLinks.thumbnail) {
     return DEFAULT_THUMBNAIL;
   }
   return imageLinks.thumbnail.replace("http://", "https://");
 };
 
-
 const drawChartBook = async (subject, startIndex = 0) => {
   let cbookContainer = document.querySelector(`.${subject}`);
   cbookContainer.innerHTML = `<div class='prompt'><div class="loader"></div></div>`;
-  const cdata = await getBooks(
-    `subject:${subject}&startIndex=${startIndex}&maxResults=10`
-  );
+  
+  const cdata = await getBooks(`subject:${subject}&startIndex=${startIndex}&maxResults=10`);
+  
   if (cdata.error) {
     cbookContainer.innerHTML = `<div class='prompt'>ツ Limit exceeded! Try after some time</div>`;
   } else if (cdata.totalItems == 0) {
@@ -34,26 +33,23 @@ const drawChartBook = async (subject, startIndex = 0) => {
   } else if (!cdata.items || cdata.items.length == 0) {
     cbookContainer.innerHTML = `<div class='prompt'>ツ There is no more result!</div>`;
   } else {
-    cbookContainer.innerHTML = cdata.items;
-    cbookContainer.innerHTML = cdata.items
-      .map(
-        ({ volumeInfo }) => 
-          `<div class='book' style='background: linear-gradient(` +
-          getRandomColor() +
-          `, rgba(0, 0, 0, 0));'><a href='${volumeInfo.previewLink}' target='_blank'><img class='thumbnail' src='` +
-          extractThumbnail(volumeInfo) +
-          `' alt='cover'></a><div class='book-info'><h3 class='book-title'><a href='${volumeInfo.previewLink}' target='_blank'>${volumeInfo.title}</a></h3><div class='book-authors' onclick='updateFilter(this,"author");'>${volumeInfo.authors}</div><div class='info' onclick='updateFilter(this,"subject");' style='background-color: ` +
-          getRandomColor() +
-          `;'>` +
-          (volumeInfo.categories === undefined
-            ? "Others"
-            : volumeInfo.categories) +
-            `</div></div><div class='actions'>
-              <button onclick='handleReadlistButtonClick("${volumeInfo.title}", "${volumeInfo.authors}", "${volumeInfo.previewLink}", "${extractThumbnail(volumeInfo)}")'>Readlist</button>
-              <button onclick='handleFavouritesButtonClick("${volumeInfo.title}", "${volumeInfo.authors}", "${volumeInfo.previewLink}", "${extractThumbnail(volumeInfo)}")'>Favourites</button>
-          </div></div>`
-      )
-      .join("");
+    cbookContainer.innerHTML = cdata.items.map(({ volumeInfo }) => {
+      return `
+        <div class='book' style='background: linear-gradient(${getRandomColor()}, rgba(0, 0, 0, 0));'>
+          <a href='${volumeInfo.previewLink}' target='_blank'><img class='thumbnail' src='${extractThumbnail(volumeInfo)}' alt='cover'></a>
+          <div class='book-info'>
+            <h3 class='book-title'><a href='${volumeInfo.previewLink}' target='_blank'>${volumeInfo.title}</a></h3>
+            <div class='book-authors' onclick='updateFilter(this,"author");'>${volumeInfo.authors}</div>
+            <div class='info' onclick='updateFilter(this,"subject");' style='background-color: ${getRandomColor()};'>
+              ${volumeInfo.categories === undefined ? "Others" : volumeInfo.categories}
+            </div>
+          </div>
+          <div class='actions'>
+            <button onclick='handleReadlistButtonClick("${volumeInfo.title}", "${volumeInfo.authors}", "${volumeInfo.previewLink}", "${extractThumbnail(volumeInfo)}")'>Readlist</button>
+            <button onclick='handleFavouritesButtonClick("${volumeInfo.title}", "${volumeInfo.authors}", "${volumeInfo.previewLink}", "${extractThumbnail(volumeInfo)}")'>Favourites</button>
+          </div>
+        </div>`;
+    }).join("");
   }
 };
 
@@ -61,7 +57,9 @@ const drawListBook = async () => {
   if (searchBooks.value != "") {
     bookContainer.style.display = "flex";
     bookContainer.innerHTML = `<div class='prompt'><div class="loader"></div></div>`;
+    
     const data = await getBooks(`${searchBooks.value}&maxResults=6`);
+    
     if (data.error) {
       bookContainer.innerHTML = `<div class='prompt'>ツ Limit exceeded! Try after some time</div>`;
     } else if (data.totalItems == 0) {
@@ -69,36 +67,30 @@ const drawListBook = async () => {
     } else if (data.totalItems == undefined) {
       bookContainer.innerHTML = `<div class='prompt'>ツ Network problem!</div>`;
     } else {
-      bookContainer.innerHTML = data.items
-        .map(
-          ({ volumeInfo }) =>
-            `<div class='book' style='background: linear-gradient(` +
-            getRandomColor() +
-            `, rgba(0, 0, 0, 0));'><a href='${volumeInfo.previewLink}' target='_blank'><img class='thumbnail' src='` +
-            extractThumbnail(volumeInfo) +
-            `' alt='cover'></a><div class='book-info'><h3 class='book-title'><a href='${volumeInfo.previewLink}' target='_blank'>${volumeInfo.title}</a></h3><div class='book-authors' onclick='updateFilter(this,"author");'>${volumeInfo.authors}</div><div class='info' onclick='updateFilter(this,"subject");' style='background-color: ` +
-            getRandomColor() +
-            `;'>` +
-            (volumeInfo.categories === undefined
-              ? "Others"
-              : volumeInfo.categories) +
-              `</div></div><div class='actions'>
-                <button onclick='handleReadlistButtonClick("${volumeInfo.title}", "${volumeInfo.authors}", "${volumeInfo.previewLink}", "${extractThumbnail(volumeInfo)}")'>Readlist</button>
-                <button onclick='handleFavouritesButtonClick("${volumeInfo.title}", "${volumeInfo.authors}", "${volumeInfo.previewLink}", "${extractThumbnail(volumeInfo)}")'>Favourites</button>
-            </div></div>`
-        )
-        .join("");
+      bookContainer.innerHTML = data.items.map(({ volumeInfo }) => {
+        return `
+          <div class='book' style='background: linear-gradient(${getRandomColor()}, rgba(0, 0, 0, 0));'>
+            <a href='${volumeInfo.previewLink}' target='_blank'><img class='thumbnail' src='${extractThumbnail(volumeInfo)}' alt='cover'></a>
+            <div class='book-info'>
+              <h3 class='book-title'><a href='${volumeInfo.previewLink}' target='_blank'>${volumeInfo.title}</a></h3>
+              <div class='book-authors' onclick='updateFilter(this,"author");'>${volumeInfo.authors}</div>
+              <div class='info' onclick='updateFilter(this,"subject");' style='background-color: ${getRandomColor()};'>
+                ${volumeInfo.categories === undefined ? "Others" : volumeInfo.categories}
+              </div>
+            </div>
+            <div class='actions'>
+              <button onclick='handleReadlistButtonClick("${volumeInfo.title}", "${volumeInfo.authors}", "${volumeInfo.previewLink}", "${extractThumbnail(volumeInfo)}")'>Readlist</button>
+              <button onclick='handleFavouritesButtonClick("${volumeInfo.title}", "${volumeInfo.authors}", "${volumeInfo.previewLink}", "${extractThumbnail(volumeInfo)}")'>Favourites</button>
+            </div>
+          </div>`;
+      }).join("");
     }
   } else {
     bookContainer.style.display = "none";
   }
 };
 
-
-
-
 const addToReadlist = async (title, authors, previewLink, thumbnail) => {
-
   try {
     const response = await fetch(`/add_to_readlist/${title}/`, {
       method: 'POST',
@@ -111,19 +103,17 @@ const addToReadlist = async (title, authors, previewLink, thumbnail) => {
         thumbnail,
       }),
     });
-    read_count += 1
+    
+    read_count += 1;
     const data = await response.json();
-    alertMessage = window.alert(data.message); // Display a message to the user (you can customize this)
-    setTimeout(() => {
-      alertMessage.close();
-    }, 1000);
+    alertMessage = window.alert(data.message);
+    setTimeout(() => { alertMessage.close(); }, 1000);
   } catch (error) {
     console.error('Error adding to Readlist:', error);
   }
 };
 
 const addToFavourites = async (title, authors, previewLink, thumbnail) => {
-
   try {
     const response = await fetch(`/add_to_favourites/${title}/`, {
       method: 'POST',
@@ -136,17 +126,15 @@ const addToFavourites = async (title, authors, previewLink, thumbnail) => {
         thumbnail,
       }),
     });
-    fav_count += 1
+    
+    fav_count += 1;
     const data = await response.json();
-    alert(data.message); // Display a message to the user (you can customize this)
+    alert(data.message);
   } catch (error) {
     console.error('Error adding to Favourites:', error);
   }
 };
 
-// ... (existing code)
-
-// Example usage in your existing code where you handle button clicks
 const handleReadlistButtonClick = (title, authors, previewLink, thumbnail) => {
   addToReadlist(title, authors, previewLink, thumbnail);
 };
@@ -155,15 +143,11 @@ const handleFavouritesButtonClick = (title, authors, previewLink, thumbnail) => 
   addToFavourites(title, authors, previewLink, thumbnail);
 };
 
-
-
-
-
-
 const updateFilter = ({ innerHTML }, f) => {
   document.getElementById("main").scrollIntoView({
     behavior: "smooth",
   });
+
   let m;
   switch (f) {
     case "author":
@@ -173,13 +157,17 @@ const updateFilter = ({ innerHTML }, f) => {
       m = "subject:";
       break;
   }
+
   searchBooks.value = m + innerHTML;
   debounce(drawListBook, 1000);
 };
+
 const debounce = (fn, time, to = 0) => {
   to ? clearTimeout(to) : (to = setTimeout(drawListBook, time));
 };
+
 searchBooks.addEventListener("input", () => debounce(drawListBook, 1000));
+
 document.addEventListener("DOMContentLoaded", () => {
   drawChartBook("inspirational");
   drawChartBook("authors");
@@ -188,13 +176,16 @@ document.addEventListener("DOMContentLoaded", () => {
   drawChartBook("fantasy");
   drawChartBook("business");
   drawChartBook("engineering");
-
 });
+
 let mainNavLinks = document.querySelectorAll(".scrolltoview");
+
 window.addEventListener("scroll", (event) => {
   let fromTop = window.scrollY + 64;
+
   mainNavLinks.forEach(({ hash, classList }) => {
     let section = document.querySelector(hash);
+
     if (
       section.offsetTop <= fromTop &&
       section.offsetTop + section.offsetHeight > fromTop
@@ -205,45 +196,42 @@ window.addEventListener("scroll", (event) => {
     }
   });
 });
-const getRandomColor = () =>
-  `#${Math.floor(Math.random() * 16777215).toString(16)}40`;
-const toggleSwitch = document.querySelector(
-  '.theme-switch input[type="checkbox"]'
-);
+
+const getRandomColor = () => `#${Math.floor(Math.random() * 16777215).toString(16)}40`;
+
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+
 if (localStorage.getItem("marcdownTheme") == "dark") {
   document.documentElement.setAttribute("data-theme", "dark");
-  document
-    .querySelector("meta[name=theme-color]")
-    .setAttribute("content", "#090b28");
+  document.querySelector("meta[name=theme-color]").setAttribute("content", "#090b28");
   toggleSwitch.checked = true;
   localStorage.setItem("marcdownTheme", "dark");
 } else {
   document.documentElement.setAttribute("data-theme", "light");
-  document
-    .querySelector("meta[name=theme-color]")
-    .setAttribute("content", "#ffffff");
+  document.querySelector("meta[name=theme-color]").setAttribute("content", "#ffffff");
   toggleSwitch.checked = false;
   localStorage.setItem("marcdownTheme", "light");
 }
+
 const switchTheme = ({ target }) => {
   if (target.checked) {
     document.documentElement.setAttribute("data-theme", "dark");
-    document
-      .querySelector("meta[name=theme-color]")
-      .setAttribute("content", "#090b28");
+    document.querySelector("meta[name=theme-color]").setAttribute("content", "#090b28");
     localStorage.setItem("marcdownTheme", "dark");
   } else {
     document.documentElement.setAttribute("data-theme", "light");
-    document
-      .querySelector("meta[name=theme-color]")
-      .setAttribute("content", "#ffffff");
+    document.querySelector("meta[name=theme-color]").setAttribute("content", "#ffffff");
     localStorage.setItem("marcdownTheme", "light");
   }
 };
+
 toggleSwitch.addEventListener("change", switchTheme, false);
+
 let startIndex = 0;
+
 const next = (subject) => {
   startIndex += 6;
+
   if (startIndex >= 0) {
     document.getElementById(`${subject}-prev`).style.display = "inline-flex";
     drawChartBook(subject, startIndex);
@@ -251,8 +239,10 @@ const next = (subject) => {
     document.getElementById(`${subject}-prev`).style.display = "none";
   }
 };
+
 const prev = (subject) => {
   startIndex -= 6;
+
   if (startIndex <= 0) {
     startIndex = 0;
     drawChartBook(subject, startIndex);
@@ -263,37 +253,45 @@ const prev = (subject) => {
   }
 };
 
-
 const modal = document.querySelector(".modal");
 const trigger = document.querySelector(".trigger");
 const closeButton = document.querySelector(".close-button");
+
 const toggleModal = () => modal.classList.toggle("show-modal");
 const windowOnClick = ({ target }) => {
   if (target === modal) {
     toggleModal();
   }
 };
+
 trigger.addEventListener("click", toggleModal);
 closeButton.addEventListener("click", toggleModal);
 window.addEventListener("click", windowOnClick);
+
 let pwaInstalled = localStorage.getItem("pwaInstalled") == "yes";
+
 if (window.matchMedia("(display-mode: standalone)").matches) {
   localStorage.setItem("pwaInstalled", "yes");
   pwaInstalled = true;
 }
+
 if (window.navigator.standalone === true) {
   localStorage.setItem("pwaInstalled", "yes");
   pwaInstalled = true;
 }
+
 if (pwaInstalled) {
   document.getElementById("installPWA").style.display = "none";
 } else {
   document.getElementById("installPWA").style.display = "inline-flex";
 }
+
 let deferredPrompt = null;
+
 window.addEventListener("beforeinstallprompt", (e) => {
   deferredPrompt = e;
 });
+
 async function installPWA() {
   if (deferredPrompt) {
     deferredPrompt.prompt();
@@ -307,6 +305,7 @@ async function installPWA() {
     });
   }
 }
+
 window.addEventListener("appinstalled", (evt) => {
   localStorage.setItem("pwaInstalled", "yes");
   pwaInstalled = true;
@@ -329,22 +328,15 @@ function previewProfilePicture() {
   }
 }
 
-// Attach the event listener to trigger the preview function when the camera icon is clicked
-document.querySelector('.upload .round i').addEventListener('click', function () {
-  document.getElementById('profilePictureInput').click();
-});
-
-
 document.addEventListener('DOMContentLoaded', function () {
   var backIcon = document.getElementById('backIcon');
+
   if (backIcon) {
-      backIcon.addEventListener('click', redirectToIndex);
+    backIcon.addEventListener('click', redirectToIndex);
   }
 
   function redirectToIndex() {
-      console.log('Icon clicked!');
-      window.location.href = "{% url 'registerApp:index' %}";
+    console.log('Icon clicked!');
+    window.location.href = "{% url 'registerApp:index' %}";
   }
 });
-
-
